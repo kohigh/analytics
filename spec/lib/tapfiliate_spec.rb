079c8ac04f144e82cc73e164fe8e51aa99c9ff59
Viacheslav Shvetsov <viacheslav@belive.tv>
2019-05-28 13:44:36 +0300

    Tapfiliate initial tracking test done;

--- spec/lib/tapfiliate_spec.rb
+++ spec/lib/tapfiliate_spec.rb
@@ -120,48 +120,6 @@ RSpec.describe Tapfiliate do
       end
     end
 
-    context 'unsuccessful' do
-      context 'error in passed params' do
-        let(:get_conversion_status) { 400 }
-        let(:get_conversion_response) { '[]' }
-
-        it 'does not make retry but provide detailed info in log' do
-          em do
-            subject
-
-            delayed(0.1) { is_expected.to loggify('ERROR', 'TAP GET /1.6/conversions/:123 vid:test, amount:0') }
-
-            done(0.1)
-          end
-        end
-      end
-
-      context 'error in passed params' do
-        let(:get_conversion_status) { 500 }
-        let(:get_conversion_response) { '[]' }
-
-        it 'does not make retry but provide detailed info in log' do
-          em do
-            subject
-
-            delayed(0.2) do
-              is_expected.to loggify(
-                               'WARN',
-                               'TAP:123 failed:1 with vid:test or amount:0',
-                               'TAP:123 failed:2 with vid:test or amount:0',
-                               'TAP:123 failed:3 with vid:test or amount:0',
-                               'ERROR',
-                               'TAP GET /1.6/conversions/:123 vid:test, amount:0'
-                             )
-              expect(a_request(:get, get_conversion_url)).to have_been_made.times(4)
-            end
-
-            done(0.2)
-          end
-        end
-      end
-
-    end
     context 'successful' do
       let(:get_conversion_status) { 200 }
       let(:get_conversion_response) { '[]' }
@@ -192,17 +150,18 @@ RSpec.describe Tapfiliate do
     let(:get_conversion_status) { 200 }
     let(:get_conversion_response) { '[{"id":1}]' }
 
+    before do
+      stub_request(:post, "https://api.tapfiliate.com/1.6/conversions/1/commissions/").
+        with(
+          body: '{"conversion_sub_amount":0}',
+          headers: {
+            'Api-Key'=>'test',
+            'Content-Type'=>'application/json'
+          }).
+        to_return(status: post_commission_status, body: "", headers: {})
+    end
     context 'successful' do
-      before do
-        stub_request(:post, "https://api.tapfiliate.com/1.6/conversions/1/commissions/").
-            with(
-                body: '{"conversion_sub_amount":0}',
-                headers: {
-                    'Api-Key'=>'test',
-                    'Content-Type'=>'application/json'
-                }).
-            to_return(status: 200, body: "", headers: {})
-      end
+      let(:post_commission_status) { 200 }
 
       it 'create commission when user has conversion in tapfiliate' do
         em do
@@ -211,6 +170,51 @@ RSpec.describe Tapfiliate do
           delayed(0.1) { is_expected.to loggify('INFO', 'TAP:123 and vid:test tracked with amount:0') }
 
           done(0.1)
+        end
+      end
+    end
+
+    context 'unsuccessful' do
+      context 'error in passed params' do
+        let(:post_commission_status) { 400 }
+
+        it 'does not make retry but provide detailed info in log' do
+          em do
+            subject
+
+            delayed(0.1) { is_expected.to loggify('ERROR', 'TAP POST /1.6/conversions/1/commissions/:123 vid:test, amount:0') }
+
+            done(0.1)
+          end
+        end
+      end
+
+      context 'returns 500 so we make n retries with exponentially growing timeout' do
+        let(:post_commission_status) { 500 }
+        let(:post_commission_url) { 'https://api.tapfiliate.com/1.6/conversions/1/commissions/' }
+
+        it 'does not make retry but provide detailed info in log' do
+          em do
+            subject
+
+            delayed(0.2) do
+              is_expected.to loggify(
+                                 'WARN',
+                                 'TAP:123 failed:1 with vid:test or amount:0',
+                                 'TAP:123 failed:2 with vid:test or amount:0',
+                                 'TAP:123 failed:3 with vid:test or amount:0',
+                                 'ERROR',
+                                 'TAP POST /1.6/conversions/1/commissions/:123 vid:test, amount:0'
+                             )
+              expect(a_request(:post, post_commission_url).with(
+                  body: '{"conversion_sub_amount":0}',
+                  headers: {'Api-Key'=>'test', 'Content-Type'=>'application/json'}
+              )).to have_been_made.times(4)
+
+            end
+
+            done(0.2)
+          end
         end
       end
     end
